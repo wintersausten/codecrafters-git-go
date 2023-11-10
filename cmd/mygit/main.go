83826847d7bb6606e83c0d2ea390f606936e0561
wintersausten <wintersausten@gmail.com>
2023-11-10 13:53:11 -0800

    outline cat-file, read & verify hash

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+  "regexp"
 )
+
+func isValidSHA1(hash string) bool {
+  // SHA1 hashes are 40 hex characters
+	if len(hash) != 40 {
+		return false
+	}
+
+	// Use a regular expression to check if the string is a valid hexadecimal
+	match, _ := regexp.MatchString("^[a-fA-F0-9]{40}$", hash)
+	return match
+}
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
 func main() {
@@ -17,15 +29,36 @@ func main() {
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 			if err := os.MkdirAll(dir, 0755); err != nil {
 				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+        return
 			}
 		}
 
 		headFileContents := []byte("ref: refs/heads/master\n")
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
 			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+      return
 		}
 
 		fmt.Println("Initialized git directory")
+
+  case "cat-file":
+    // verify & parse hash
+    if len(os.Args) != 3 {
+      fmt.Fprintf(os.Stderr, "usage: mygit cat-file <hash>")
+      return
+    }
+
+    hash := os.Args[2]
+    if !isValidSHA1(hash) {
+      fmt.Fprintf(os.Stderr, "The provided hash could not be verified, please provide a valid SHA1 hash")
+      return
+    }
+
+    dir, file := hash[:2], hash[2:]
+    // read file in
+    // decompress
+    // read decompressed data
+    // output data
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
