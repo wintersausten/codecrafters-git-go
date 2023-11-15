5ce94a5bdc769d10733c4e1f88bb5a4e7b034ae0
wintersausten <wintersausten@gmail.com>
2023-11-14 18:41:26 -0800

    add hash object subcommand

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
-  "bytes"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"regexp"
-  "errors"
-  "flag"
 )
 
 func isValidSHA1(hash string) bool {
@@ -35,6 +39,8 @@ func main() {
     err = initGit()
   case "cat-file":
     err = catFile(os.Args[2:])
+  case "hash-object":
+    err = hashObject(os.Args[2:])
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
@@ -63,12 +69,10 @@ func initGit() error {
   return nil
 }
 
+var catFileCmd = flag.NewFlagSet("cat-file", flag.ExitOnError)
+var pFlag = catFileCmd.Bool("p", false, "Pretty print based on object type")
+
 func catFile(args []string) error {
-  catFileCmd := flag.NewFlagSet("cat-file", flag.ExitOnError)
-
-  // Define flags for the cat-file subcommand
-  pFlag := catFileCmd.Bool("p", false, "Some flag description")
-
   // Parse flags for the cat-file command
   err := catFileCmd.Parse(args)
   if err != nil {
@@ -76,59 +80,143 @@ func catFile(args []string) error {
       return err
   }
 
-  // Check if the -p flag is provided
-  if *pFlag {
-    if catFileCmd.NArg() < 1 {
-      fmt.Fprintf(os.Stderr, "usage: mygit cat-file -p {hash}\n")
-      return err
-    }
-
-    hash := catFileCmd.Arg(0)
-    if !isValidSHA1(hash) {
-      fmt.Fprintf(os.Stderr, "The provided hash could not be verified, please provide a valid SHA1 hash\n")
-      return err
-    }
-
-    dir, file := hash[:2], hash[2:]
-
-    // open compressed blob 
-    blobPath := fmt.Sprintf(".git/objects/%s/%s", dir, file)
-    compressedBlob, err := os.Open(blobPath)
-    if err != nil {
-      if errors.Is(err, os.ErrNotExist) {
-        fmt.Fprintf(os.Stderr, "The file corresponding to the hash %s does not exist.\n", hash)
-      } else {
-        fmt.Fprintf(os.Stderr, "Error opening file: %s\n", err)
-      }
-      return err
-    }   
-    defer compressedBlob.Close()
-
-    // set up blob decompression
-    decompressedBlob, err := zlib.NewReader(compressedBlob)
-    if err != nil {
-      fmt.Fprintf(os.Stderr, "Error decompressing file: %s\n", err)
-      return err
-    }
-    defer decompressedBlob.Close()
-
-    // write decompressed blob to stdout
-    blobData, err := io.ReadAll(decompressedBlob)
-    if err != nil {
-      fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
-      return err
-    }
-
-    // check for header & parse out file content
-    nullCharIndex := bytes.IndexByte(blobData, '\x00')
-    if nullCharIndex < 0 {
-      fmt.Fprintf(os.Stderr, "Error reading file, no header found\n")
-      return err
-    }
-
-    content := blobData[nullCharIndex+1:]
-
-    fmt.Print(string(content))
+  if catFileCmd.NArg() != 1 {
+    fmt.Fprintf(os.Stderr, "usage: mygit cat-file {flags} {hash}\n")
+    return err
   }
+
+  hash := catFileCmd.Arg(0)
+  if !isValidSHA1(hash) {
+    fmt.Fprintf(os.Stderr, "The provided hash could not be verified, please provide a valid SHA1 hash\n")
+    return err
+  }
+
+  objectData, err := readObject(hash)
+  if err != nil {
+    return err
+  }
+
+  if *pFlag {
+    // && assumed object type is blob
+    err := printBlob(objectData)
+    if err != nil {
+      return err
+    }
+  }
+    return nil
+}
+
+func printBlob (objectData []byte) error {
+  
+  // parse out header
+  // TODO: move this into the object read (also create an object abstraction)
+  nullCharIndex := bytes.IndexByte(objectData, '\x00')
+  if nullCharIndex < 0 {
+    fmt.Fprintf(os.Stderr, "Error reading file, no header found\n")
+    return errors.New("No header found in object file")
+  }
+
+  content := objectData[nullCharIndex+1:]
+
+  fmt.Print(string(content))
+  return nil
+}
+
+func readObject(hash string) ([]byte, error) {
+  dir, file := hash[:2], hash[2:]
+  // TODO: use filepath
+  objectPath := fmt.Sprintf(".git/objects/%s/%s", dir, file)
+
+  // open compressed file 
+  compressedObject, err := os.Open(objectPath)
+  if err != nil {
+    if errors.Is(err, os.ErrNotExist) {
+      fmt.Fprintf(os.Stderr, "The object corresponding to the hash %s does not exist.\n", hash)
+    } else {
+      fmt.Fprintf(os.Stderr, "Error opening file: %s\n", err)
+    }
+    return nil, err
+  }   
+  defer compressedObject.Close()
+
+  // setup decompressor
+  decompressedObject, err := zlib.NewReader(compressedObject)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Error decompressing file: %s\n", err)
+    return nil, err
+  }
+  defer decompressedObject.Close()
+
+  // read the file data
+  // consider using bufio vs io.ReadAll if processing larger files
+  objectData, err := io.ReadAll(decompressedObject)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+    return nil, err
+  }
+  
+  return objectData, nil
+}
+
+var hashObjectCmd = flag.NewFlagSet("hashObject", flag.ExitOnError)
+var wFlag = hashObjectCmd.Bool("w", false, "Write the object")
+
+func hashObject(args []string) error {
+  err := hashObjectCmd.Parse(args)
+  if err != nil {
+      fmt.Fprintf(os.Stderr, "Error parsing flags for hash-object: %s\n", err)
+      return err
+  }
+
+  if hashObjectCmd.NArg() != 1 {
+    fmt.Fprintf(os.Stderr, "usage: mygit hash-object {flags} {file}\n")
+    return err
+  }
+  fileName := hashObjectCmd.Arg(0)
+
+  // read the file in to mem
+  file, err := os.Open(fileName)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Error opening file.")
+    return err
+  }
+  // TODO: replace with bufio to handle larger files
+  fileContents, err := io.ReadAll(file)
+
+  // add header
+  header := []byte("header")
+  header = append(header, '\x00')
+  object := append(header, fileContents...)
+
+  // hash file file contents 
+  hasher := sha1.New()
+  hasher.Write(object)
+  hash := hex.EncodeToString(hasher.Sum(nil))
+
+  // if w flag, Write
+  if *wFlag {
+    dir, file := hash[:2], hash[2:]
+    path := filepath.Join("./.git/objects", dir, file)
+
+    if _, err := os.Stat(path); errors.Is(err, fs.ErrNotExist) {
+      err := os.MkdirAll(dir, os.ModePerm)
+      if err != nil {
+        fmt.Fprintf(os.Stderr, "Error creating directory: %s", err)
+        return err
+      }
+    }
+
+    objectFile, err := os.Create(path)
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "Error creating file: %s", err)
+      return err
+    }
+    defer objectFile.Close()
+
+    compressedObjectWriter := zlib.NewWriter(objectFile)
+    compressedObjectWriter.Write(object)
+  }
+
+  fmt.Print(hash)
   return nil
 }
